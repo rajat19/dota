8556865681c23bbeff2f24c1c1853f6f748d94d5
rajat19 <rajatsri94@gmail.com>
2026-01-30 15:37:03 +0530

    fix routing

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,15 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+// Set basename for GitHub Pages deployment
+// In production, the app is served from /dota/
+const basename = import.meta.env.PROD ? '/dota' : '/'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={basename}>
             <App />
         </BrowserRouter>
     </React.StrictMode>,
 )
+
